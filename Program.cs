52f8ed3c1cb57ad213df76ab31468eec1c6cdf6a
luuizfelipe <luiz.nascimentodev96@gmail.com>
2024-06-24 21:07:11 -0300

    Projeto em finalização

--- Program.cs
+++ Program.cs
@@ -1,7 +1,54 @@
-﻿internal class Program
+﻿using tabuleiro;
+using xadrez;
+
+internal class Program
 {
-    private static void Main(string[] args)
+    static void Main(string[] args)
     {
-       
+
+        try
+        {
+
+            PartidaDeXadrez partida = new PartidaDeXadrez();
+
+            while (!partida.terminada)
+            {
+                try
+                {
+
+
+                    Console.Clear();
+                    Tela.imprimirPartida(partida);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
+
+                    bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
+
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                    partida.realizaJogada(origem, destino);
+                }
+                catch (TabuleiroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }
+            }
+        }
+        catch (TabuleiroException e)
+        {
+            Console.WriteLine(e.Message);  
+        }
+        Console.ReadLine();
     }
 }
